59fc3df1d84f8d3e3fa227350848bdafc7b7ac52
dmihovch <dmihovch@gmail.com>
2025-04-13 12:26:05 -0400

    Setup Pages && more cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
-import Home from './pages/Home'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import Blog from "./pages/Blog";
+import Projects from "./pages/Projects";
+import Resume from "./pages/Resume";
+import NotFound from "./pages/NotFound";
 
 function App() {
-
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,7 @@
+import { useNavigate } from "react-router-dom";
+
+function Navbar() {
+  const nav = useNavigate();
+}
+
+export default Navbar;
